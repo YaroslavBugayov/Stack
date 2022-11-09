f5e98e7c7116387525d834a6fd1e58faec4be6df
YaroslavBugayov <bugayov576@gmail.com>
2022-11-09 20:32:34 +0200

    it's not a lot but it's honest work

---
+++ Stack/ConsoleApp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class ConsoleApp
+    {
+        public static void Main()
+        {
+            Stack<int> stack = new Stack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            int[] array = stack.ToArray();
+            foreach (int i in array)
+            {
+                Console.Write(i);
+            }
+            Console.WriteLine(stack.Count());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            //foreach (int i in stack)
+            //{
+            //    Console.WriteLine(i);
+            //}
+        }
+        
+    }
+}
---
+++ Stack/Stack.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Stack
+{
+    public class Stack<T> : IEnumerable<T>, ICollection
+    {
+        private LinkedList<T> _data = new LinkedList<T>();
+
+        public void Push(T elem)
+        {
+            _data.AddFirst(elem);
+        }
+
+        public T Peek()
+        {
+            return _data.First();
+        }
+
+        public T Pop()
+        {
+            T elem = _data.First();
+            _data.RemoveFirst();
+            return elem;
+        }
+
+        public int Count
+        {
+            get { return _data.Count; }
+        }
+
+        public T[] ToArray()
+        {
+            return _data.ToArray();
+        }
+        public bool IsSynchronized => throw new NotImplementedException();
+
+        public object SyncRoot => throw new NotImplementedException();
+
+        public void CopyTo(Array array, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            foreach (var datum in _data)
+            {
+                yield return datum;
+            }
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            return (IEnumerator<T>)GetEnumerator();
+        }
+    }
+
+}
