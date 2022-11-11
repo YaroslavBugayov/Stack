536b213d925012b671bf405c1aff18b0c34148a6
YaroslavBugayov <bugayov576@gmail.com>
2022-11-11 14:41:01 +0200

    done

--- Stack/ConsoleApp.cs
+++ Stack/ConsoleApp.cs
@@ -10,29 +10,47 @@ namespace Stack
     {
         public static void Main()
         {
-            Stack<int> stack = new Stack<int>();
-            stack.Push(1);
-            stack.Push(2);
-            stack.Push(3);
-            stack.Push(4);
-            stack.Push(5);
+            Stack<string> stack = new Stack<string>();
+            //stack.Push("1");
+            //stack.Push("2");
+            //stack.Push("3");
+            //stack.Push("4");
+            //stack.Push("5");
+            //stack.Push(null);
+            stack.Pop();
+            //Console.WriteLine(stack.Peek());
+            //Console.WriteLine(stack.Contains(2));
+            //Console.WriteLine(stack.Contains(6));
+
+            //Console.WriteLine("Count: " + stack.Count);
             //stack.Clear();
+            //Console.WriteLine("Count: " + stack.Count);
+
             //stack.Push(6);
             //Console.WriteLine("Pop: " + stack.Pop());
             //Console.WriteLine("Pop: " + stack.Pop());
             //Console.WriteLine("Peek: " + stack.Peek());
-            //int[] array = stack.ToArray();
-            //foreach (int i in array)
+            //Console.WriteLine(stack.Count);
+            //Console.WriteLine(stack.Pop());
+            //Console.WriteLine(stack.Pop());
+
+            //foreach (string i in stack)
             //{
-            //    Console.Write(i);
+            //    Console.WriteLine(i);
             //}
-            //Console.WriteLine(stack.Count());
-            //Console.WriteLine(stack.Pop());
-            //Console.WriteLine(stack.Pop());
-            foreach (int i in stack)
-            {
-                Console.WriteLine(i);
-            }
+
+            //foreach (int i in stack.ToArray())
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //int[] testArray = new int[stack.Count*2];
+            //stack.CopyTo(testArray, 0);
+            //stack.CopyTo(testArray, stack.Count);
+            //foreach (int i in testArray)
+            //{
+            //    Console.WriteLine(i);
+            //}
         }
         
     }
--- Stack/Stack/Stack.cs
+++ Stack/Stack/Stack.cs
@@ -5,63 +5,106 @@ namespace Stack.Stack
 {
     public class Stack<T> : IEnumerable<T>, ICollection
     {
-        Node<T> current;
-        int count;
+        private object _syncRoot;
+        private Node<T> _current;
+        private int _count;
 
         public void Push(T data)
         {
+            if (data == null) { throw new ArgumentNullException("data"); }
             Node<T> node = new Node<T>(data);
 
-            current.Next = current;
-            current = node;
-            count++;
+            _current.Next = _current;
+            _current = node;
+            _count++;
         }
 
         public T Peek()
         {
-            return current.Data;
+            if (_count == 0) { throw new InvalidOperationException("empty"); }
+            return _current.Data;
         }
 
         public T Pop()
         {
-            current = current.Next;
-            count--;
-            return current.Data;
+            if (_count == 0) { throw new InvalidOperationException("empty"); }
+            _current = _current.Next;
+            _count--;
+            return _current.Data;
         }
 
         public int Count
         {
-            get { return count; }
+            get { return _count; }
         }
 
-        public T[] ToArray()
+        public Array ToArray()
         {
-            return new T[0];
+            T[] nodes = new T[_count];
+            Node<T> tempNode = _current;
+            while(_current != null)
+            {
+                nodes.Append(tempNode.Data);
+                tempNode = _current.Next;
+            }
+            return nodes;
         }
 
         public void Clear()
         {
-            current = null;
-            count = 0;
+            _current = null;
+            _count = 0;
         }
 
-        public void TrimExcess()
+        public bool Contains(T data)
         {
-            
+            if (data != null) { throw new ArgumentNullException("data"); }
+            Node<T> tempNode = _current;
+            bool isContain = false;
+            while (_current != null)
+            {
+                if (_current.Data.Equals(data))
+                {
+                    isContain = true;
+                }
+                tempNode = _current.Next;
+            }
+            return isContain;
         }
 
-        public bool IsSynchronized => throw new NotImplementedException();
-
-        public object SyncRoot => throw new NotImplementedException();
+        public bool IsSynchronized
+        {
+            get { return false; }
+        }
+        
+        public object SyncRoot
+        {
+            get
+            {
+                Interlocked.CompareExchange(ref _syncRoot, new object(), null);
+                return _syncRoot;
+            }
+        }
 
         public void CopyTo(Array array, int index)
         {
-            throw new NotImplementedException();
+            if (array == null) { throw new ArgumentNullException("array"); }
+            if (index < 0) { throw new ArgumentNullException ("index"); }
+            if (array.Rank != 1) { throw new ArgumentException("rank"); }
+            if (_current == null) { throw new InvalidOperationException("empty"); }
+            if (array.Length - index < _count) { throw new ArgumentOutOfRangeException("array"); }
+
+            Node<T> tempNode = _current;
+            while (_current != null)
+            {
+                array.SetValue(tempNode.Data, index++);
+                tempNode = _current.Next;
+            }        
         }
 
         public IEnumerator GetEnumerator()
         {
-            Node<T> node = current;
+            Node<T> node = _current;
             while (node != null)
             {
                 yield return node.Data;
@@ -72,6 +115,25 @@ namespace Stack.Stack
         IEnumerator<T> IEnumerable<T>.GetEnumerator()
         {
             return (IEnumerator<T>)GetEnumerator();
+        }
+
+        public event EventHandler<Node<T>> EventPush = delegate { };
+        public event EventHandler<Node<T>> EventPop = delegate { };
+        public event Action EventClear = delegate { };
+
+        protected virtual void OnPush(Node<T> data)
+        {
+            EventPush.Invoke(this, data);
+        }
+
+        protected virtual void OnPop(Node<T> data)
+        {
+            EventPop.Invoke(this, data);
+        }
+
+        protected virtual void OnClear()
+        {
+            EventClear.Invoke();
         }
     }
 
